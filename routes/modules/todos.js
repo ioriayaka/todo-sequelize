0b88bd48c2850f622397e7394ddda1c57e7f4d8b
ioriayaka <ioriayaka@hotmail.com>
2021-08-17 10:22:35 +0800

    add create todo function

--- routes/modules/todos.js
+++ routes/modules/todos.js
@@ -3,7 +3,20 @@ const router = express.Router()
 
 const db = require('../../models')
 const Todo = db.Todo
+//新增資料
+router.get('/new', (req, res) => {
+  return res.render('new')
+})
 
+router.post('/', (req, res) => {
+  const UserId = req.user.id
+  const name = req.body.name
+
+  return Todo.create({ name, UserId })
+    .then(() => res.redirect('/'))
+    .catch(error => console.log(error))
+})
+//查詢單筆資料
 router.get('/:id', (req, res) => {
   const UserId = req.user.id
   const id = req.params.id
